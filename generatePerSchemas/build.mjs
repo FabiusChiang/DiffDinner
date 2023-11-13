3b19217aa96b39b576f3af7096809286f9121b51
Fabius Jiang <fabiushust@hotmail.com>
2023-11-13 11:16:46 -0600

    export/import issue is resolved

--- generatePerSchemas/build.mjs
+++ generatePerSchemas/build.mjs
@@ -6,8 +6,18 @@ console.log(filesList);
 
 let indexTsContent = 'export function func1 (a: number, b: number): number {return a + b;};\n';
 
+function convertEntityName(fileName) {
+    const firstLetter = fileName[0].toUpperCase();
+    if (fileName.length > 1) {
+        return firstLetter + fileName.slice(1, fileName.length);
+    }
+    else {
+        return firstLetter;
+    }
+}
+
 filesList.forEach(element => {
-    indexTsContent = indexTsContent + `export * from \'${element.outputFolderName}${element.entityName}';\n`
+    indexTsContent = indexTsContent + `export \{ ${convertEntityName(element.entityName)} \} from \'${element.outputFolderName}${element.entityName}';\n`
     compileFromFile(element.inputFolderName + element.jsonFileName)
         .then(ts => fs.writeFileSync(element.outputFolderName + element.tsFileName, ts));
 });
--- generatePerSchemas/index.ts
+++ generatePerSchemas/index.ts
@@ -1,8 +1,4 @@
-export {Cuisine} from './types/cuisine';
-export {Meal} from './types/meal';
-
-function func1(a: number, b: number): number {
-    return a + b;
-}
-
-export { func1 };
+export function func1 (a: number, b: number): number {return a + b;};
+export { Cuisine } from './types/cuisine';
+export { Material } from './types/material';
+export { Meal } from './types/meal';
